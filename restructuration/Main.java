7c6966a69db92f7b15ce3cf60918e22ccec4d21a
El-Harrougui MOHAMED <elharrougui0@gmail.com>
2023-10-11 13:17:45 +0100

    Create Main.java

---
+++ restructuration/Main.java
@@ -0,0 +1,14 @@
+package restructuration;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+        System.out.println( "restructuration start" );
+        
+        System.out.println( ">Arg1: " + args[0] );
+        System.out.println( ">Arg2: " + args[1] );
+
+        System.out.println( "restructuration end" );
+
+    }
+}
